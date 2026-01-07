b666c127b7ae6c0220310291051215e3e7a3e349
Zac Israel <zac390@gmail.com>
2026-01-07 11:30:49 -0500

    feat(bootcamp service): add getBootcampById method

--- src/services/bootcamp.service.ts
+++ src/services/bootcamp.service.ts
@@ -1,5 +1,6 @@
 // This file handles the interaction between the API & the bootcamp collection in MongoDB
 
+import type { ObjectId } from "mongoose";
 import type { CreateBootcampDTO } from "../dtos/bootcamp.dto.js";
 import { Bootcamp } from "../models/bootcamp.model.js";
 import type { BootcampType } from "../types/bootcamp.interface.js";
@@ -33,5 +34,14 @@ export const bootcampService = {
   async getAllBootcamps(): Promise<BootcampType[]> {
     // retrieves all of the bootcamp documents from the bootcamp MongoDB collection
     return await Bootcamp.find();
+  },
+
+  // Retrieves a bootcamp with a given id
+  async getBootcampById(id: string): Promise<BootcampType | null> {
+    const bootcamp = await Bootcamp.findById(id);
+
+    // return retrieved bootcamp
+    // if it's null, that means it does not exist; gets handled in the controller
+    return bootcamp;
   },
 };
