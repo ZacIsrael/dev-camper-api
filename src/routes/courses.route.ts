b769d80af69e69ca10eba5f1ef3206e5d9e7cabf
Zac Israel <zac390@gmail.com>
2026-01-14 09:40:35 -0500

    feat(routes): add courses route to handle GET course by id

--- src/routes/courses.route.ts
+++ src/routes/courses.route.ts
@@ -1,13 +1,16 @@
 // Import the Express framework for building HTTP servers
 import express from "express";
 
-import { getCourses } from "../controllers/courses.controller.js";
+import { getCourseById, getCourses } from "../controllers/courses.controller.js";
 
 
 const router = express.Router();
 
 // Get all courses
 router.get("/", getCourses);
+
+// Get course by id
+router.get("/:id", getCourseById)
 
 
 export default router;
