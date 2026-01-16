f52621c32ee228f777ffb288c57497ca3d1b15f6
Zac Israel <zac390@gmail.com>
2026-01-15 22:54:34 -0500

    feat(types): add UserType interface to define MongoDB user document structure

---
+++ src/types/user.interface.ts
@@ -0,0 +1,32 @@
+// This interface defines the structure of a User MongoDB Document.
+// In other words, it reflects exactly what a document entry
+// looks like in the "users" collection in MongoDB.
+
+
+/*
+
+    Example User structure:
+
+    {
+		"_id": "5d7a514b5d2c12c7449be042",
+		"name": "Admin Account",
+		"email": "admin@gmail.com",
+		"role": "user",
+		"password": "123456"
+	}
+
+*/
+
+// MongoDB module
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface UserType extends Document {
+  // When creating a User, _id doesn’t exist yet. But when reading or updating users, it will.
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  // enum: "user" or "publisher"
+  role: string;
+  password: string;
+
+}
