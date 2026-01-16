e6e52a4e5974e5d87a5e3c2dfb2541f897086b2a
Zac Israel <zac390@gmail.com>
2026-01-15 22:55:48 -0500

    feat(model): add User mongoose model with schema validation and role enum

---
+++ src/models/user.model.ts
@@ -0,0 +1,48 @@
+// MongoDB module
+import mongoose, { Schema, model } from "mongoose";
+
+import type { UserType } from "../types/user.interface.js";
+
+/*
+
+    Example User structure:
+
+    {
+		"_id": "5d7a514b5d2c12c7449be042",
+		"name": "Admin Account",
+		"email": "admin@gmail.com",
+		"role": "user",
+		"password": "123456"
+	}
+
+*/
+
+// schema for "users" collection
+const userSchema = new Schema<UserType>({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    // can't have the same name as another bootcamp document in the collection
+    unique: true,
+    // remove leading and trailing white space
+    trim: true,
+    maxLength: [50, "Name can't be longer than 50 characters"],
+  },
+  email: {
+    type: String,
+    required: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please add a valid email"],
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["user", "publisher"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// create and export this User model
+export const User = model<UserType>("User", userSchema);
