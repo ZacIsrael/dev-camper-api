b5232567df25bda9c6a229fff2dfdbdab4bee600
Zac Israel <zac390@gmail.com>
2026-02-20 13:55:34 -0500

    feat(auth-routes): change resetPassword method to PATCH and add protected updateDetails endpoint for profile updates

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,7 +6,8 @@ import {
   login,
   getMe,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updateDetails,
 } from "../controllers/auth.controller.js";
 import { addCourse, getCourses } from "../controllers/courses.controller.js";
 
@@ -16,7 +17,7 @@ const router = express.Router();
 
 // Register a user
 router.post("/register", register);
-// User can login 
+// User can login
 router.post("/login", login);
 
 // Retrieves the user that's currently logged in
@@ -26,6 +27,9 @@ router.get("/me", protect, getMe);
 router.post("/forgotpassword", forgotPassword);
 
 // Executed when a user resets their password
-router.put("/resetpassword/:resettoken", resetPassword);
+router.patch("/resetpassword/:resettoken", resetPassword);
+
+// Executed when a user wants to update their name and/or email
+router.patch("/updatedetails", protect, updateDetails);
 
 export default router;
