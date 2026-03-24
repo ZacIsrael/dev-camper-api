0abe8e52736941ad7b3687a3f740d15e0d17abcb
Zac Israel <zac390@gmail.com>
2026-03-24 15:25:49 -0400

    feat(review-routes): add route parameter validation for review resource routes

--- src/routes/reviews.route.ts
+++ src/routes/reviews.route.ts
@@ -9,7 +9,10 @@ import {
   deleteReview,
 } from "../controllers/reviews.controller.js";
 
-import { protect, authorize } from "../middleware/auth.middleware.js";
+import { validateParams } from "../middleware/validate.middleware.js";
+import { IdParamDTO } from "../dtos/params.dto.js";
+
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -17,12 +20,12 @@ const router = express.Router();
 router.get("/", getReviews);
 
 // Get review by id
-router.get("/:id", getReviewById);
+router.get("/:id", validateParams(IdParamDTO), getReviewById);
 
 // Update a review
-router.patch("/:id", protect, updateReview);
+router.patch("/:id", validateParams(IdParamDTO), protect, updateReview);
 
 // Delete a review (only the user that wrote the review or an admin should be able to delete it)
-router.delete("/:id", protect, deleteReview);
+router.delete("/:id", validateParams(IdParamDTO), protect, deleteReview);
 
 export default router;
