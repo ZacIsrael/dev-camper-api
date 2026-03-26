3e28dce7e0df32403e08ea39e7d20bcf817c63ec
Zac Israel <zac390@gmail.com>
2026-03-26 12:37:51 -0400

    fix(course-dto): restore user field in CreateCourseDTO to align with current controller logic

--- src/dtos/course.dto.ts
+++ src/dtos/course.dto.ts
@@ -27,8 +27,7 @@ export class CreateCourseDTO {
   tuition: number;
   minimumSkill: MinimumSkill;
   scholarhipsAvailable: boolean;
-  // user should not come from the client!
-  // user: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
 
   constructor(data: unknown) {
     const payload = assertIsObject(data, "Request body must be a valid object");
@@ -90,11 +89,11 @@ export class CreateCourseDTO {
       throw new Error("Please add a user id");
     }
 
-    // if (!mongoose.Types.ObjectId.isValid(payload.user)) {
-    //   throw new Error("Please add a valid user id");
-    // }
+    if (!mongoose.Types.ObjectId.isValid(payload.user)) {
+      throw new Error("Please add a valid user id");
+    }
 
-    // this.user = new mongoose.Types.ObjectId(payload.user);
+    this.user = new mongoose.Types.ObjectId(payload.user);
   }
 }
 
